9721aba2ce410a2fe21ed162d069a685b3e8e138
Eva-Renata <all2evi@yahoo.com>
2021-09-02 09:33:30 +0200

    pageloader2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,45 @@
+
 window.addEventListener("load", function () {
     const loader = document.querySelector(".loader");
     loader.className += " hidden"; // class "loader hidden"
 });
+
+// Formvalidering - Jens
+
+const message = document.getElementById('message-wrap')
+const inputs = document.querySelectorAll(':required')
+const reset = document.getElementById('reset')
+
+reset.addEventListener('click', function() {
+    location.reload()
+})
+
+function validate(){
+    let status = true
+    for(let input of inputs){
+        if(!input.value){
+            setError(input)
+            status = false
+            return false
+        }
+    }
+if(status) {
+        message.style.display = "block"
+        message.style.color = "black"
+        message.innerText = "Formularen er sendt"
+    }
+}
+
+function setError(element){
+    
+    message.innerText =`Udfyld venligst feltet ${element.previousElementSibling.innerText}`
+    message.style.display = "block"
+
+    element.focus()
+
+    element.addEventListener('keydown', function(){
+        if(message.style.display = "block"){
+            message.style.display = "none"
+        }
+    })
 }
